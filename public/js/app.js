7e20dfd53d9ebc3712baf4c23a64f84deee2df5a
Jasper-Keong <keongjh@gmail.com>
2022-08-16 16:49:28 +0800

    added translate
    translate

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,6 @@ var genre_url = "/genre_details";
 var genre_array = []; // This creates an empty genre array
 var genreCount = 0;
 var currentIndex = 0;
+
+var translate_url = new URL("https://bkkq9e63u1.execute-api.ap-southeast-1.amazonaws.com/dev");
+var newText = "";
