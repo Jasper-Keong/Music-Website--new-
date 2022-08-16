b4489ff615f18e366d6695f64b7e12d94b9279f1
Jasper-Keong <83157204+Jasper-Keong@users.noreply.github.com>
2022-08-16 16:52:07 +0800

    Update db-connections.js

--- db-connections.js
+++ db-connections.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    //host: 'mysqlinstance.c1klrnqbqiee.ap-southeast-1.rds.amazonaws.com',
+    //host: 'localhost',
+    host: 'mysqlinstance.c1klrnqbqiee.ap-southeast-1.rds.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'mysql255',
-    //user: 'admin',
-    //password: 'testing255',
+    //user: 'root',
+    //password: 'mysql255',
+    user: 'admin',
+    password: 'testing255',
     database: 'music_website'
 });
 
